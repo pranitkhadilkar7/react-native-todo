9d1e7692d1de0645038a9149ddfd9b8190e491e2
PranitK <Pranit.Khadilkar@talentica.com>
2022-08-07 14:21:45 +0530

    improving layout

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal!!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals....</Text>
       </View>
     </View>
@@ -16,11 +16,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 2,
+    marginBottom: 16,
   },
   textInput: {
     borderWidth: 1,
@@ -28,5 +35,8 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 })
