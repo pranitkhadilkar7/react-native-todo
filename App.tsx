22ba05221ff5ecb8fa21c803d3a2c86966a9a8b0
PranitK <Pranit.Khadilkar@talentica.com>
2022-08-07 12:08:31 +0530

    using flexbox to create layouts

--- App.tsx
+++ App.tsx
@@ -1,26 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Hello from React Native App!!</Text>
-      <Text style={styles.dummyText}>Another Text</Text>
-      <Button title="Tap me!!" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals....</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    flexGrow: 1,
+    marginRight: 8,
     padding: 8,
   },
 })
